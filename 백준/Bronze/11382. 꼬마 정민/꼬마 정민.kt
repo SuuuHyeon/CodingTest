5eb6a8141545f55f4a654be9b1d999f993a8e2f8
Subari <127729464+SuuuHyeon@users.noreply.github.com>
2025-03-30 00:02:05 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 15252 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.kt
@@ -0,0 +1,7 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val result = sc.nextLine().split(" ")
+    println(result[0].toLong() + result[1].toLong() + result[2].toLong())
+}
