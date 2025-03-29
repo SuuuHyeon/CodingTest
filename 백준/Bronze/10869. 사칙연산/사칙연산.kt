d510d46c6fa47cb6f48ebe61a51ab88deb8d2b92
Subari <127729464+SuuuHyeon@users.noreply.github.com>
2025-03-29 23:06:17 +0900

    [Bronze V] Title: 사칙연산, Time: 100 ms, Memory: 13272 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    val input = Scanner(System.`in`)
+    val num1 = input.nextInt()
+    val num2 = input.nextInt()
+    println(num1 + num2)
+    println(num1 - num2)
+    println(num1 * num2)
+    println(num1 / num2)
+    println(num1 % num2)
+}
