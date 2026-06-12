1e5f084337b7154b6a4efd4f72287cb4fe5f2945
suhyeon <127729464+SuuuHyeon@users.noreply.github.com>
2026-06-13 00:02:45 +0800

    YOJ 2869. 달팽이는 올라가고 싶다 [java]

---
+++ 백준/2869.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long V = Long.parseLong(st.nextToken());
+
+        long answer = (V - B + (A - B) - 1) / (A - B);
+
+        System.out.println(answer);
+    }
+}
